2458a631a4f40fbc6befae2156936b78acf6ebc9
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-03-26 02:45:19 +0200

    Updated configs

--- src/configs/envVariables.js
+++ src/configs/envVariables.js
@@ -1,9 +1,9 @@
 exports.SERVER_NAME = 'Theatrix';
-exports.PORT = 3000;
+exports.PORT = 3001;
 exports.DB_CONNECTION = `mongodb://127.0.0.1:27017/${this.SERVER_NAME}DB`;
 exports.SALTS = 10; // default - 10
 exports.SECRET = '!D!e09i 0n384,s-57vmay3985lhnd95lvn34957vbna738509yl-220fmwni';
-exports.TOKEN_EXPIRATION = '150m';
+exports.TOKEN_EXPIRATION = '15m';
 exports.SESSION_COOKIE_NAME = 'user';
 exports.USER_MIN_STRENGTH = 3;
 exports.PASS_MIN_STRENGTH = 8;
